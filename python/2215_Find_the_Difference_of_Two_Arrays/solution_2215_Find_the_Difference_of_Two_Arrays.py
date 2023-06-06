04658fad70e4f410df7101cd5759d259730590e7
balart40 <ng711690@iteso.mx>
2023-06-06 14:16:02 -0700

    https://leetcode.com/problems/find-the-difference-of-two-arrays

---
+++ python/2215_Find_the_Difference_of_Two_Arrays/solution_2215_Find_the_Difference_of_Two_Arrays.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def findDifference(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[List[int]]
+        """
+        set1 = set(nums1)
+        set2 = set(nums2)
+        left = set1 - set2
+        right = set2 - set1
+        return [list(left), list(right)]
