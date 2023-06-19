edb30fb6bd8dc6bd9bf8064d24aa6eaa19b0edf4
balart40 <ng711690@iteso.mx>
2023-06-19 11:47:57 -0700

    https://leetcode.com/problems/find-peak-element/

---
+++ python/162_Find_Peak_Element/solution_162_Find_Peak_Element.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        if n == 1:
+            return 0
+        low = 0
+        high  = n - 1
+        while low < high:
+            mid = (low + high) // 2
+            if nums[mid] > nums[mid + 1]:
+                high = mid
+            else:
+                low = mid + 1
+        return low
