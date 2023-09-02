6d3c11d393281a30fb51ba70a478a2b7141e915b
balart40 <ng711690@iteso.mx>
2023-09-02 01:33:25 -0700

    reduce code using deque

--- python3/28_Find_the_Index_of_the_First_Occurrence_in_a_String/solution_28_Find_the_Index_of_the_First_Occurrence_in_a_String.py
+++ python3/28_Find_the_Index_of_the_First_Occurrence_in_a_String/solution_28_Find_the_Index_of_the_First_Occurrence_in_a_String.py
@@ -2,28 +2,18 @@ class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         needle_len = len(needle)
         haystack_len = len(haystack)
-        stack =  [i for i in range(haystack_len) if haystack[i] == needle[0]]
+        queue = collections.deque()
 
-        if not stack:
+        for i in range(haystack_len):
+            if haystack[i] == needle[0]:
+                queue.append(i)
+
+        if not queue:
             return -1
 
-        def search_ocurrence(j: int) -> int:
-            curr_idx = 0
-            idx = -1
-            for i in range(j, haystack_len):
-                if haystack[i] == needle[curr_idx]:
-                    if curr_idx == 0:
-                        idx = i
-                    curr_idx += 1
-                    if curr_idx == needle_len:
-                        return idx
-                else:
-                    return -1
-            return -1
-
-        while stack:
-            i = stack.pop(0)
-            result = search_ocurrence(i)
+        while queue:
+            i = queue.popleft()
+            result = i if haystack[i:i+needle_len] == needle else -1
             if result != -1:
                 return result
         return -1
