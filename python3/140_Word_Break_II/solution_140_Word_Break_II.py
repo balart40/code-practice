a8716a1ef4130c8640cd3bd7b013ab08218a4afb
balart40 <ng711690@iteso.mx>
2023-09-09 20:33:06 -0700

    https://leetcode.com/problems/word-break-ii/

---
+++ python3/140_Word_Break_II/solution_140_Word_Break_II.py
@@ -0,0 +1,17 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        result = []
+        def word_break_rec(s, start, current_word):
+            nonlocal result
+            nonlocal wordDict
+            if start == len(s) and current_word:
+                result.append(' '.join(current_word))
+            for i in range(start, len(s)):
+                word = s[start : i + 1]
+                if word in wordDict:
+                    current_word.append(word)
+                    word_break_rec(s, i + 1, current_word)
+                    current_word.pop()
+
+        word_break_rec(s, 0, [])
+        return result
