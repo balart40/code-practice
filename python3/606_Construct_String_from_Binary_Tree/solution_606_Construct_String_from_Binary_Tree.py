2b490ec51f6cd1c98cfd18dfd0b8f4c86fc15188
balart40 <ng711690@iteso.mx>
2023-12-08 09:14:01 -0800

    return pre_order_rec(root)git

---
+++ python3/606_Construct_String_from_Binary_Tree/solution_606_Construct_String_from_Binary_Tree.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def tree2str(self, root: Optional[TreeNode]) -> str:
+
+        def pre_order_rec(node):
+            if not node: return
+            result = str(node.val)
+            if node.left:
+                result += "(" + pre_order_rec(node.left) + ")"
+            if node.right:
+                if not node.left: result += "()"
+                result += "("+ pre_order_rec(node.right) + ")"
+            return result
+        return pre_order_rec(root)
