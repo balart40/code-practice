6941d113506edcab037a79d3ff9edd9d70e96215
balart40 <ng711690@iteso.mx>
2023-12-30 21:50:55 -0800

    https://leetcode.com/problems/largest-substring-between-two-equal-characters/

---
+++ python3/1624_Largest_Substring_Between_Two_Equal_Characters/solution_1624_Largest_Substring_Between_Two_Equal_Characters.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxLengthBetweenEqualCharacters(self, s: str) -> int:
+        result = -1
+        d = collections.defaultdict(int)
+        for i in range(len(s)):
+            if s[i] in d:
+                result = max(result, i - d[s[i]] - 1)
+            else:
+                d[s[i]] = i
+        return result
