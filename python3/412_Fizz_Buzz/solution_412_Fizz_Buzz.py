5eaffaed87aefccaf7d304c914c8a10e7dfdd877
balart40 <ng711690@iteso.mx>
2024-01-15 18:49:09 -0800

    additional if to reduce code

--- python3/412_Fizz_Buzz/solution_412_Fizz_Buzz.py
+++ python3/412_Fizz_Buzz/solution_412_Fizz_Buzz.py
@@ -2,10 +2,11 @@ class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
         result = []
         for i in range(1, n + 1):
-            if(i % 3 == 0 and i % 5 == 0):
-                result.append(str("FizzBuzz"))
-            elif (i % 3 == 0):
-                result.append(str("Fizz"))
+            if(i % 3 == 0):
+                if(i % 5 == 0 ):
+                    result.append(str("FizzBuzz"))
+                else:
+                    result.append(str("Fizz"))
             elif (i % 5 == 0):
                 result.append(str("Buzz"))
             else:
